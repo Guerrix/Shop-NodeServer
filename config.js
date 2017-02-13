94137ce2bab4ad7f584c447c5c618bb853177b89
Jesus Guerra <jguerrix@gmail.com>
2017-02-13 16:46:44 -0700

    Add secret token

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/shop'
+  db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+  SECRET_TOKEN: 'fsdfs#$TYRGTr675^JTHGnWE%#$5^&I*(JSFDf@#$Ty%&^h@#)'
 }
