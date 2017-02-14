f8f34b4787fd81f5cd1c1591058bb928fa0635c6
Jesus Guerra <jguerrix@gmail.com>
2017-02-13 17:27:29 -0700

    Protect endpoints with webtoken

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@
 
 const express = require('express')
 const productConstroller = require('../controllers/product')
+const userConstroller = require('../controllers/auth')
 const api = express.Router()
+const auth = require('../middlewares/auth')
 
-api.get('/product', productConstroller.getProducts)
-api.get('/product/:productId', productConstroller.getProduct)
-api.post('/product', productConstroller.createProduct)
-api.put('/product/:productId', productConstroller.updateProduct)
-api.delete('/product/:productId', productConstroller.deleteProduct)
+api.post('/signUp', userConstroller.signUp)
+
+api.get('/product', auth.isAuth, productConstroller.getProducts)
+api.get('/product/:productId', auth.isAuth, productConstroller.getProduct)
+api.post('/product', auth.isAuth, productConstroller.createProduct)
+api.put('/product/:productId', auth.isAuth, productConstroller.updateProduct)
+api.delete('/product/:productId', auth.isAuth, productConstroller.deleteProduct)
 
 module.exports = api
