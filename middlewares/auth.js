07d88e619a34ff54e3d1aff9f982578091132db2
Jesus Guerra <jguerrix@gmail.com>
2017-02-13 17:24:03 -0700

    Make methods a callback

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,23 @@ const jwt = require('jwt-simple')
 const moment = require('moment')
 const config = require('../config')
 
-function isAuth (req, res, next) {
+// function isAuth (req, res, next) {
+//   if (!req.headers.authorization) {
+//     return res.status(403).send({message: 'User not athorized'})
+//   }
+//
+//   const token = req.headers.authorization.split(' ')[1]
+//   const payload = jwt.decode(token, config.SECRET_TOKEN)
+//
+//   if (payload.exp <= moment().unix()) {
+//     return res.status(401).send({message: 'Token expired'})
+//   }
+//
+//   req.user = payload.sub
+//   next()
+// }
+
+module.exports = {isAuth: function (req, res, next) {
   if (!req.headers.authorization) {
     return res.status(403).send({message: 'User not athorized'})
   }
@@ -12,12 +28,10 @@ function isAuth (req, res, next) {
   const token = req.headers.authorization.split(' ')[1]
   const payload = jwt.decode(token, config.SECRET_TOKEN)
 
-  if (payload.exp < moment().unix()) {
+  if (payload.exp <= moment().unix()) {
     return res.status(401).send({message: 'Token expired'})
   }
 
   req.user = payload.sub
   next()
-}
-
-module.exports = isAuth
+}}
