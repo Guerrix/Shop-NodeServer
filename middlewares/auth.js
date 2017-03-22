37a74a13824d629552873cf5c0ee0aa56a6fdd8e
Jesus Guerra <jguerrix@gmail.com>
2017-03-21 19:42:07 -0700

    Use jsonwebtoken instead jwt-simple
    basically to solve exception

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const jwt = require('jwt-simple')
+const jwt = require('jsonwebtoken')
 const moment = require('moment')
 const config = require('../config')
 
@@ -26,12 +26,15 @@ module.exports = {isAuth: function (req, res, next) {
   }
 
   const token = req.headers.authorization.split(' ')[1]
-  const payload = jwt.decode(token, config.SECRET_TOKEN)
-
-  if (payload.exp <= moment().unix()) {
-    return res.status(401).send({message: 'Token expired'})
+  try {
+    const payload = jwt.verify(token, config.SECRET_TOKEN)
+    if (payload.exp <= moment().unix()) {
+      return res.status(401).send({message: 'Token expired'})
+    }
+    
+    req.user = payload.sub
+    next()
+  } catch (err) {
+    return res.status(401).send({message: err.message})
   }
-
-  req.user = payload.sub
-  next()
 }}
